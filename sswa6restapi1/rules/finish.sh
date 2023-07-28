9e0b9924988a6f629a0cc93b8b26bae6338e2870
SAlAstakhov <s3k4@yandex.ru>
2023-07-28 13:00:07 +0300

    1

--- sswa6restapi1/rules/finish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-httpbin_retcode="$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:32100/httpbin/get)"
-
-jq --null-input \
---arg httpbin_retcode "$httpbin_retcode" \
-'{"httpbin_retcode": $httpbin_retcode}'| jq -n '.results |= [inputs]'
