d279e1d5ea80ec5c2d9582104026ac41e11651ad
SAlAstakhov <s3k4@yandex.ru>
2023-07-28 13:31:21 +0300

    1

--- sswa7restapi1/assets/prepare.sh
+++ sswa7restapi1/assets/prepare.sh
@@ -21,7 +21,7 @@ function install_ingress() {
     kubectl -n ingress-nginx wait --for=condition=available --timeout=3m deployment/ingress-nginx-controller
     test $? -eq 1 && echo "[ERROR] Ingress controller not ready" && kill "$!" && exit 1
     kubectl -n ingress-nginx patch svc ingress-nginx-controller --patch \
-      '{"spec": { "type": "NodePort", "ports": [ { "nodePort": 4010, "port": 80, "protocol": "TCP", "targetPort": 80 } ] } }'
+      '{"spec": { "type": "NodePort", "ports": [ { "nodePort": 32100, "port": 80, "protocol": "TCP", "targetPort": 80 } ] } }'
 
     echo done
     touch $INGRESS_DONE
