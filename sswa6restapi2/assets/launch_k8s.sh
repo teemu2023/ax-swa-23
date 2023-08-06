8e39690cc05003656dc955a20396f4299477803b
SAlAstakhov <s3k4@yandex.ru>
2023-08-06 14:26:35 +0300

    1

--- sswa6restapi2/assets/launch_k8s.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-launch.sh
-
-while [ $(kubectl get nodes -o jsonpath='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'| tr ';' "\n"  | grep "Ready=True" | wc -l) != "2" ] ; do sleep 1 ; done
