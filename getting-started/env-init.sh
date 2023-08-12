4fd74a010bbd1bd3d418ca56e18a10b09f23e872
SAlAstakhov <s3k4@yandex.ru>
2023-08-12 14:37:33 +0300

    1

--- getting-started/env-init.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# This is supposed to run in the background to provision your workload
-# ssh root@host01 "mkdir -p /root/my-example”
--- sc5/install_istio.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -sL https://istio.io/downloadIstioctl | sh -
-export PATH=$PATH:$HOME/.istioctl/bin
-istioctl x precheck
-istioctl install
