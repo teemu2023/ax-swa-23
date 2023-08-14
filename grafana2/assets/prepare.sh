bcc71d04f6c33cf0335ed3e375b0d0e47a4ef705
SAlAstakhov <s3k4@yandex.ru>
2023-08-14 17:49:12 +0300

    1

--- grafana2/assets/prepare.sh
+++ grafana2/assets/prepare.sh
@@ -37,132 +37,86 @@ function install_grafana() {
 
   if [ ! -f "$GRAFANA_DONE" ]; then
   cat <<EOF >/tmp/grafana.yaml
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: grafana-pvc
+spec:
+  accessModes:
+    - ReadWriteOnce
+  resources:
+    requests:
+      storage: 1Gi
 apiVersion: apps/v1
 kind: Deployment
 metadata:
-  name: grafana
   labels:
     app: grafana
+  name: grafana
 spec:
-  replicas: 1
   selector:
     matchLabels:
       app: grafana
   template:
     metadata:
-      name: grafana
       labels:
         app: grafana
     spec:
       securityContext:
         fsGroup: 472
-        runAsGroup: 472
-        runAsNonRoot: true
-        runAsUser: 472
+        supplementalGroups:
+          - 0
       containers:
-      - name: grafana
-        image: grafana/grafana:7.3.7
-        imagePullPolicy: IfNotPresent
-        ports:
         - name: grafana
-          containerPort: 3000
-        resources:
-          limits:
-            memory: "2Gi"
-            cpu: "1000m"
-          requests: 
-            memory: "1Gi"
-            cpu: "500m"
-        volumeMounts:
-          - name: grafana-config
-            mountPath: /etc/grafana/grafana.ini
-            subPath: grafana.ini
-
-          - name: grafana-storage
-            mountPath: /var/lib/grafana
-
-          - name: grafana-certs
-            mountPath: /etc/grafana/certs/
-            readOnly: true
-
-          - name: grafana-datasources
-            mountPath: /etc/grafana/provisioning/datasources/prometheus.yaml
-            subPath: prometheus.yaml
-
-          - name: grafana-dashboard-providers
-            mountPath: /etc/grafana/provisioning/dashboards/dashboardproviders.yaml
-            subPath: dashboardproviders.yaml
-
-          - name: dashboards-k8s-cluster-summary
-            mountPath: /var/lib/grafana/dashboards/k8s-cluster-summary
-
-          - name: dashboards-node-exporter-full
-            mountPath: /var/lib/grafana/dashboards/node-exporter-full
-
-      # See https://kubernetes.io/docs/concepts/workloads/pods/init-containers/
-      #initContainers:
-      #  - name: fix-nfs-permissions
-      #    image: busybox
-      #    command: ["sh", "-c", "chown -R 472:472 /var/lib/grafana"]
-      #    securityContext:
-      #      runAsUser: 0
-      #      runAsNonRoot: false
-      #    volumeMounts:
-      #      - name: grafana-storage
-      #        mountPath: /var/lib/grafana/
-
-      restartPolicy: Always
-      terminationGracePeriodSeconds: 60
+          image: grafana/grafana:latest
+          imagePullPolicy: IfNotPresent
+          ports:
+            - containerPort: 3000
+              name: http-grafana
+              protocol: TCP
+          readinessProbe:
+            failureThreshold: 3
+            httpGet:
+              path: /robots.txt
+              port: 3000
+              scheme: HTTP
+            initialDelaySeconds: 10
+            periodSeconds: 30
+            successThreshold: 1
+            timeoutSeconds: 2
+          livenessProbe:
+            failureThreshold: 3
+            initialDelaySeconds: 30
+            periodSeconds: 10
+            successThreshold: 1
+            tcpSocket:
+              port: 3000
+            timeoutSeconds: 1
+          resources:
+            requests:
+              cpu: 250m
+              memory: 750Mi
+          volumeMounts:
+            - mountPath: /var/lib/grafana
+              name: grafana-pv
       volumes:
-        - name: grafana-config
-          configMap:
-            name: grafana-ini
-
-        - name: grafana-storage
+        - name: grafana-pv
           persistentVolumeClaim:
-            claimName: nfs-pvc-grafana
-
-        - name: grafana-certs
-          secret:
-            secretName: grafana-tls-cert
-            items:
-            - key: tls.crt
-              path: tls.crt
-            - key: tls.key
-              path: tls.key
-
-        - name: grafana-datasources
-          configMap:
-            name: grafana-datasources
-
-        - name: grafana-dashboard-providers
-          configMap:
-            name: grafana-dashboard-providers
-
-        - name: dashboards-k8s-cluster-summary
-          configMap:
-            name: grafana-dashboards-k8s-cluster-summary
-
-        - name: dashboards-node-exporter-full
-          configMap:
-            name: grafana-dashboards-node-exporter-full
+            claimName: grafana-pvc
 apiVersion: v1
 kind: Service
 metadata:
   name: grafana
-  annotations:
-    prometheus.io/scrape: 'true'
-    prometheus.io/port:   '3000'
-  labels:
-    app: grafana
 spec:
-  selector: 
-    app: grafana
-  type: NodePort  
+  type: NodePort
   ports:
-    - port: 3000
-      targetPort: 3000
-      nodePort: 32100          
+  - name: http
+    port: 3000
+    targetPort: 3000
+    nodePort: 32100
+  selector:
+    app: grafana
+  sessionAffinity: None
 EOF
     kubectl apply -f /tmp/grafana.yaml
     kubectl wait --for=condition=ContainersReady --timeout=5m --all pods
