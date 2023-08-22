27b85abfbbaa4f27ecf6e09aae36d5f2530743ac
SAlAstakhov <s3k4@yandex.ru>
2023-08-22 15:52:20 +0300

    1

--- argocd1/assets/get_repo_url.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get the remote URL of the origin
-local_git_repo_url=$(git config --get remote.origin.url)
-
-# Print the local Git repository URL
-echo "Local Git Repository URL: $local_git_repo_url"
